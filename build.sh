664280dd74bfe19a8cbf31aea53b5ccbc6a3c338
Mateusz <uszek619@gmail.com>
2026-05-13 13:59:36 +0200

    haslo

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('uszek', 'uszek619@gmail.com', 'Mati1906!') if not User.objects.filter(username='admin').exists() else None"
